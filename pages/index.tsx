82cdf7f235cd095783fc84ce06997cf5a2871ce1
feus <barca105@nate.com>
2022-06-03 14:52:18 +0900

    메인 페이지. 시작 안함

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,38 @@
 import type {NextPage} from 'next'
-import React from "react";
-import SubmitForm from "../components/submitForm";
-import TitleText from "../components/titleText";
-
+import React, {SyntheticEvent, useEffect, useState} from "react";
+import AOS from "aos";
+import "aos/dist/aos.css"
+import router from 'next/router';
+import Link from 'next/link';
 
 const Home: NextPage = () => {
+
+	const [go, setGo] = useState<boolean>(false);
+
+	useEffect(() => {
+		AOS.init({ duration: 1000 });
+
+		setGo(true);
+	}, [])
+
+
+	const onClick = (event: SyntheticEvent<HTMLButtonElement>) => {
+		console.log(event.currentTarget.value)
+		router.push(`/${event.currentTarget.value}`)
+	}
 
 
 	return (
 		<>
-			<TitleText/>
-			<SubmitForm ok accept cancel/>
+			<div className={`text-4xl sm:text-8xl font-semibold w-auto text-center my-8 select-none ${go ? `translate-x-5` : null} duration-1000`}>
+				Ordinary Space.
+			</div>
+			<Link href="/calendar">
+				<a className="flex justify-center items-center w-full text-2xl hover:text-green-400 transition-colors">
+					calander
+				</a>
+			</Link>
+			{/*<SubmitForm/>*/}
 		</>
 	)
 }
