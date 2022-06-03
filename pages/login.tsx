f37430d558b174e114ea27b388b2c96a4b77468c
feus <barca105@nate.com>
2022-06-03 14:52:43 +0900

    로그인 페이지. 시작 안함

---
+++ pages/login.tsx
@@ -0,0 +1,12 @@
+export default function Login() {
+	return (
+		<div className="flex flex-col justify-center">
+			<div className="w-auto mx-5 my-10 text-center text-2xl">
+				Connect Ordinary Space.
+			</div>
+			<div className="w-auto mx-5 h-72 border-white border rounded">
+				로그인폼
+			</div>
+		</div>
+	)
+}
